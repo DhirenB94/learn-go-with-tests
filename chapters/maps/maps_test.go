ec3a83ddb8010cc1636e794a8da1a0bfb17623ed
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-12-06 11:12:48 +0000

    maps - word not found in dictionary gives error

--- chapters/maps/maps_test.go
+++ chapters/maps/maps_test.go
@@ -7,11 +7,27 @@ import (
 
 func TestSearch(t *testing.T) {
 	dictionary := maps.Dictionary{"test": "this is just a test"}
+	t.Run("word not in dictionary", func(t *testing.T) {
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+		_, err := dictionary.Search("test2")
+		if err == nil {
+			t.Fatal("expected an error but did not get one")
+		}
+		want := "word not in dictionary"
 
-	assertEqualStrings(t, got, want)
+		assertEqualStrings(t, err.Error(), want)
+		// Errors can be converted to a string with the .Error() method
+	})
+	t.Run("word is in dictionary", func(t *testing.T) {
+		got, err := dictionary.Search("test")
+		if err != nil {
+			t.Fatal("should not have an error but got one")
+		}
+		want := "this is just a test"
+
+		assertEqualStrings(t, got, want)
+	})
+
 }
 
 func assertEqualStrings(t testing.TB, got, want string) {
