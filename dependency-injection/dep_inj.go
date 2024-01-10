f0971e1fe38d16a76aa67826809f2a028cc4b137
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2024-01-10 11:27:52 +0000

    dep inj - refactor to use io.writer instead of a buffer in the func

--- dependency-injection/dep_inj.go
+++ dependency-injection/dep_inj.go
@@ -1,11 +1,11 @@
 package dep
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	//PrintF writes to stdout, but we need to write to the buffer
 	//fmt.Printf("Hello, %s", name)
 
