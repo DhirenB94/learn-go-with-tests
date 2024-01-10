a9035684d6ed0ae80aaa41480e032b0d04829bb8
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2024-01-10 12:10:40 +0000

    mocking - time.sleep in the func

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -12,6 +13,7 @@ const coutdownStart = 3
 func Countdown(writer io.Writer) {
 	for i := coutdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
+		time.Sleep(time.Second * 1)
 	}
 	fmt.Fprint(writer, finalWord)
 }
