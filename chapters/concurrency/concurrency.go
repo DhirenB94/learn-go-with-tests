4195adbe55072dee8b571b26104cff0525e3ec31
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2024-01-12 16:56:32 +0000

    concurrency - introducing go routines to the CheckWebsites func (failing test)

--- chapters/concurrency/concurrency.go
+++ chapters/concurrency/concurrency.go
@@ -7,8 +7,15 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		//each iteration of the loop will run in its own go routine
+		go func() {
+			results[url] = wc(url)
+		}()
 	}
 
 	return results
 }
+
+//Anonymous functions
+//The () at the end of the anon func allows them to be executed at the same time they are declared (as opposed to habving to call them somewhere)
+// All the variables that are available at the point when you declare the anonymoous function, are also available in the body of the func
