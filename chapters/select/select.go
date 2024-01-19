6a42a726f7adee4c63d64b78c5eb0505d54229ba
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2024-01-19 12:31:31 +0000

    select - introduce select and channels to the WebsiteRacer

--- chapters/select/select.go
+++ chapters/select/select.go
@@ -2,26 +2,37 @@ package selct
 
 import (
 	"net/http"
-	"time"
 )
 
 //Make a function called WebsiteRacer which takes two URLs and "races" them by hitting them with an HTTP GET and returning the URL which returned first.
 //If none of them return within 10 seconds then it should return an error.
 
+// instead of testing speeds of the sites one after the other, check both at the same time with channels
+// exact timings dont matter, just care about which comes first
 func WebsiteRacer(a, b string) string {
-	aDuration := measureDuration(a)
-	bDuration := measureDuration(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureDuration(url string) time.Duration {
-	startTime := time.Now()
-	http.Get(url)
-	duration := time.Since(startTime)
+//SELECT AND CHANNELS
+// variable := <- channel ----> this is a BLOCKING call as you are wating for a value
+// select allows us to wait on multiple channels, the first one to recieve a value "wins" and the code under the "case" is executed - in this case return
+// we are setting up 2 channels via the ping func, which ever channel finishes the http.get call 1st, unblocks and executes.
 
-	return duration
+// ping creates a channel struct and returns it when it recieves a signal (closing of the channel)
+// it is a channel struct because we dont care about the type thats sent to the channel
+// and channel struct is the smallest data type from a memory point of view
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	//as soon as http.Get is completed, the channel is instructed to close and returned
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
